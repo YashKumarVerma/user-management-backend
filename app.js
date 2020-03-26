bb00dccdb9a884d84f8b1d92d07479f3303831ce
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-03-26 22:01:34 +0530

    Remove comments

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
 const app = require('express')()
-// const MongoClient = require('mongodb').MongoClient
 
 const compression = require('compression')
 const sanitizer = require('express-sanitizer')
 const parser = require('body-parser')
 const morgan = require('./logging/morgan')
 const database = require('./modules/mongoose/connect')
-
-// // Connect to the database
-// const dbConnectionString =
-//   "mongodb://localhost:27017/smoketrees" || process.env.DB_CONN_STRING;
-// const dbConn = async () => {
-//   try {
-//     const dbPromise = MongoClient.connect(dbConnectionString, {
-//       useUnifiedTopology: true
-//     });
-//     app.locals.dbClient = await dbPromise;
-//   } catch (e) {
-//     console.log(e);
-//     process.exit(2);
-//   }
-// };
-// dbConn().then(() => {});
 
 // Middlewares
 app.use(sanitizer())
