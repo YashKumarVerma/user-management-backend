395391356b5b542dcef8e648d56f7afe043696fb
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-02 05:32:25 +0530

    Login Route Written
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -1,6 +1,8 @@
 const logger = require('./../../logging/logger')
 const UserSchema = require('./user.schema')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('./config')
 
 const saltRounds = 10
 
@@ -149,7 +151,7 @@ class UserOperations {
           return {
             status: false,
             error: true,
-            message: 'user not found user',
+            message: 'user not found user'
           }
         }
         return {
@@ -169,6 +171,43 @@ class UserOperations {
           value: err
         }
       })
+  }
+
+  static login (user) {
+    if (user.username && user.password) {
+      UserSchema.find({ username: user.username, password: bcrypt.hashSync(user.password, saltRounds) }).then((user) => {
+        if (!user) {
+          return {
+            status: false,
+            error: true,
+            message: 'User not found'
+          }
+        } else {
+          const token = jwt.sign({ username: user.username },
+            config.secret,
+            {
+              expiresIn: '24h'
+            }
+          )
+          return {
+            status: true,
+            error: false,
+            message: 'Authentication successful!',
+            token: token
+          }
+        }
+      })
+        .catch((err) => {
+          logger.error('error', err)
+          return {
+            status: false,
+            error: true,
+            type: 'database',
+            message: 'error finding user',
+            value: err
+          }
+        })
+    }
   }
 }
 
