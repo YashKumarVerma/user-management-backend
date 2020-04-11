ea22642d0975eecb20ea10360f5428a26f187fa7
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-11 18:38:56 +0530

    login route client stub add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- client-stub/client.js
+++ client-stub/client.js
@@ -1,15 +1,12 @@
 const fetch = require('node-fetch')
-const bcrypt = require('bcrypt')
 const agent = require('./agent')
 const url = require('./url')
 
-const saltRounds = 10
-
 class UserClient {
-  async signup (user) {
+  async signup (user, bcryptPwd) {
     const res = await fetch(url + '/signup', {
       method: 'POST',
-      body: JSON.stringify({ newuser: { id: user._id, username: user.username, password: bcrypt.hashSync(user.password, saltRounds) } }),
+      body: JSON.stringify({ newuser: { id: user._id, username: user.username, email: user.email, password: bcryptPwd } }),
       headers: { 'Content-Type': 'application/json' },
       agent
     })
@@ -22,6 +19,17 @@ class UserClient {
       method: 'POST',
       body: JSON.stringify({ newuser: { userobj: user } }),
       headers: { Authorization: token, 'Content-Type': 'application/json' },
+      agent
+    })
+    const body = await res.json()
+    return body
+  }
+
+  async login (user, hashres) {
+    const res = await fetch(url + '/login', {
+      method: 'POST',
+      body: JSON.stringify({ auth: { id: user._id, email: user.email, username: user.username, hash: hashres } }),
+      headers: { 'Content-Type': 'application/json' },
       agent
     })
     const body = await res.json()
