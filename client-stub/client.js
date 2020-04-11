9238fac2b5e6da9966565a65044c943daf24bf11
lordtt13 <thakurtanmay72@yahoo.com>
2020-04-11 18:14:35 +0530

    Code refactored to class style
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- client-stub/client.js
+++ client-stub/client.js
@@ -1,8 +1,21 @@
-const draft = require('./draft')
+const fetch = require('node-fetch')
+const bcrypt = require('bcrypt')
+const agent = require('./agent')
+const url = require('./url')
 
-const ForumClient = function () {
+const saltRounds = 10
+
+class UserClient {
+  async signup (user) {
+    const res = await fetch(url + '/signup', {
+      method: 'POST',
+      body: JSON.stringify({ id: user._id, user: { username: user.username, password: bcrypt.hashSync(user.password, saltRounds) } }),
+      headers: { 'Content-Type': 'application/json' },
+      agent
+    })
+    const body = await res.json()
+    return body
+  }
 }
 
-ForumClient.prototype.newDraft = draft.newDraft
-
-module.exports = new ForumClient()
+module.exports = UserClient
